71f92284e56dd7caf31285dc3f5310134a9c8a37
limpanda7 <limpanda7@naver.com>
2022-03-08 16:36:30 +0900

    - demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './App.scss';
+import {list1, list2} from './List.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [type, setType] = useState('1');
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        switch (type) {
+            case '1':
+                setList(list1);
+                return;
+            case '2':
+                setList(list2);
+                return;
+            default:
+                return;
+        }
+    }, [type])
+
+    return (
+        <div className="App">
+
+            <div className='Header'>
+                <h1>라이브러리</h1>
+                <select onChange={(e) => setType(e.target.value)}>
+                    <option value='1'>1번타입</option>
+                    <option value='2'>2번타입</option>
+                </select>
+            </div>
+
+            <div className='Content'>
+                <table>
+                    <tr>
+                        <th>번호</th>
+                        <th>구분</th>
+                        <th>제목</th>
+                        <th>링크</th>
+                    </tr>
+                    {
+                        list.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{index + 1}</td>
+                                    <td>{item.category}</td>
+                                    <td className='Title'>{item.title}</td>
+                                    <td><a href={item.link} target='_blank'>바로가기</a></td>
+                                </tr>
+                            );
+                        })
+                    }
+                </table>
+            </div>
+
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/List.js
@@ -0,0 +1,30 @@
+const list1 = [
+    {
+        'category': '1분',
+        'title': '결정적인 증거',
+        'link': 'https://wordsquare.org'
+    },
+    {
+        'category': '1분',
+        'title': '뭔지도 모르면서?',
+        'link': 'https://wordsquare.org'
+    },
+]
+
+const list2 = [
+    {
+        'category': '유쾌한 강사',
+        'title': '왜 원망하죠',
+        'link': 'https://wordsquare.org'
+    },
+    {
+        'category': '신서의 역사',
+        'title': '이미 이렇게 될 걸 알고 있었다!',
+        'link': 'https://wordsquare.org'
+    },
+]
+
+export {
+    list1,
+    list2,
+}
