615f291c4e0cf0052ba2b9a113d61760fd35a92c
Sunghoon Lim <limpanda7@gmail.com>
2022-03-09 21:56:26 +0900

    - demo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {list1, list2} from './List.js';
 
 function App() {
 
-    const [type, setType] = useState('1');
+    const [type, setType] = useState('0');
     const [list, setList] = useState([]);
 
     useEffect(() => {
@@ -24,34 +24,71 @@ function App() {
         <div className="App">
 
             <div className='Header'>
-                <h1>라이브러리</h1>
+                <h1>말씀광장 라이브러리</h1>
                 <select onChange={(e) => setType(e.target.value)}>
-                    <option value='1'>1번타입</option>
-                    <option value='2'>2번타입</option>
+                    <option value='0'>선택하세요</option>
+                    <option value='1'>무신앙용(오픈예정자)</option>
+                    <option value='2'>무신앙용(오픈)</option>
                 </select>
             </div>
 
             <div className='Content'>
-                <table>
-                    <tr>
-                        <th>번호</th>
-                        <th>구분</th>
-                        <th>제목</th>
-                        <th>링크</th>
-                    </tr>
-                    {
-                        list.map((item, index) => {
-                            return (
-                                <tr key={index}>
-                                    <td>{index + 1}</td>
-                                    <td>{item.category}</td>
-                                    <td className='Title'>{item.title}</td>
-                                    <td><a href={item.link} target='_blank'>바로가기</a></td>
-                                </tr>
-                            );
-                        })
-                    }
-                </table>
+
+                {
+                    type === '0' &&
+                    <div className='Description'>
+                        ❣️링크 보내는 tip<br/>
+                        청년) " 나요즘 신천지에서 하는 컨텐츠 댓글리뷰 해주고있는데  은근 흥미로운거 같아 ! 사실 알고지낸 애가 갑자기 자기 신천지라면서 해달라고 하더라구 . 근데 이상한건 없는것 같더라~  손해볼거 없어서 해주고 있어."<br/>
+                        베댓(베스트댓글) 당첨되면 커피쿠폰 날라옴!!<br/><br/>
+
+                        장년/부녀) 우리 딸이 이걸 보내줬는데 무슨 내용같냐고 물어보더니 신천지교회에서 만든거라고 하네요.  이상한 교회인줄 알았는데 아닌거 같다면서 봐보라는데 같이 한번 봅시다 ~!<br/><br/>
+
+                        오픈섭자 청년 ) 우리교회에서 만들고있는 컨텐츠들인데 한번 봐줄수있어?<br/>
+                        하도 사람들이 여기는 하나님 예수님을 안믿고 교주 믿는다고 오해를 하니까 ㅠㅠ<br/>
+                        무엇을 믿고,  말하고자 하는게 뭔지 쉽게 알려주기 위해 만들고 있거든~!  이걸보고 객관적으로 어떻게 느껴지나 궁금한데 ..코멘트 한마디만 달아주면 좋겠어!<br/><br/>
+
+                        오픈섭자 장년/부녀) 우리교회 청년들이 만든 영상인데 한번 봐줄수 있어요?~ 하도 요즘 사람들이 오해가 많아서 말이죠 ..<br/>
+                        신천지교회는 하나님, 예수님을 분명하게 믿고  신앙하는 교회거든요.<br/>
+                        영상보고나서 어땠는지 한마디만 적어주면 더 좋은 영상을 만들수 있다고 하니까 시간나면 부탁해요~^^
+                    </div>
+                }
+                {
+                    type === '1' &&
+                    <div className='Description'>
+                        🖤무신앙용(오픈예정자 or 오픈했으나 관심없는 유형):  >>1분성경 (※주의: 영상안에 S가 언급되진 않으나 링크를 자세히 들어가면 오픈될 수 있습니다. 따라서 오픈해도 상관없는 지인에게 보내는 것을 권장합니다. )
+                    </div>
+                }
+                {
+                    type === '2' &&
+                    <div className='Description'>
+                        🤍무신앙용(오픈): 유쾌한 강사/신서의역사/바이블프리즘<br/>
+                        ※ 영상끝에 튜터링 광고가 포함되어 있습니다
+                    </div>
+                }
+
+                {
+                    type !== '0' &&
+                    <table>
+                        <tr>
+                            <th></th>
+                            <th>구분</th>
+                            <th>제목</th>
+                            <th>링크</th>
+                        </tr>
+                        {
+                            list.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td className='Category'>{item.category}</td>
+                                        <td className='Title'>{item.title}</td>
+                                        <td><a href={item.link} target='_blank'>▶</a></td>
+                                    </tr>
+                                );
+                            })
+                        }
+                    </table>
+                }
             </div>
 
 
--- src/List.js
+++ src/List.js
@@ -1,27 +1,133 @@
 const list1 = [
     {
-        'category': '1분',
-        'title': '결정적인 증거',
-        'link': 'https://wordsquare.org'
+        'category': '1분성경',
+        'title': '신이 지금의 우주를 만들었다는 결정적인 증거',
+        'link': 'https://www.wordsquare.org/video-yard/videos/616447ac71a8a10013688702'
     },
     {
-        'category': '1분',
-        'title': '뭔지도 모르면서?',
-        'link': 'https://wordsquare.org'
+        'category': '1분성경',
+        'title': '천국이 뭔지도 모르면서 천국엘 간다고요?',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61644ab171a8a10013688703'
+    },
+    {
+        'category': '1분성경',
+        'title': '끝장 면접! 천국행 면접 시작합니다.',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61644b3c71a8a10013688704'
+    },
+    {
+        'category': '1분성경',
+        'title': '보이지 않는데 어떻게 믿어요?',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61644b7e71a8a10013688705'
+    },
+    {
+        'category': '1분성경',
+        'title': '지옥을 믿지 않는 당신에게 성경이 보내는 경고',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61644d4271a8a10013688706'
+    },
+    {
+        'category': '1분성경',
+        'title': '못 하는 게 아니라 안 하는 거야! 하나님이 사단을 두고 보시는 이유?',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617946d2e3f64e001584f4b4'
+    },
+    {
+        'category': '1분성경',
+        'title': '거짓말 같은 진짜 러브스토리, 이런 사랑 놓치지 않는 법',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61794afbe3f64e001584f4b5'
+    },
+    {
+        'category': '1분성경',
+        'title': '\'우연\'을 맹신하는 당신에게 보여주고 싶은 이야기',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61794d57e3f64e001584f4b6'
+    },
+    {
+        'category': '1분성경',
+        'title': '천지를 지었지만 내 집은 없으셨던 하나님의 내 집 되찾기!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617abab7e3f64e001584f507'
+    },
+    {
+        'category': '1분성경',
+        'title': '에덴동산에서 재림 때 시온산까지, 천국변천사 한 방에 정리!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617ac462e3f64e001584f513'
+    },
+    {
+        'category': '1분성경',
+        'title': '모르고 있으면 부끄러운 성경을 믿을 수 있는 이유!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617c0132e3f64e001584f516'
+    },
+    {
+        'category': '1분성경',
+        'title': '아무리 애를 써도 당신은 심판을 피할 수 없다!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617c02fce3f64e001584f517'
+    },
+    {
+        'category': '1분성경',
+        'title': '천국 다녀오신 분, 손 좀 들어보세요~',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617c0439e3f64e001584f518'
+    },
+    {
+        'category': '1분성경',
+        'title': '당신이 무슨 짓을 해도 알 수 없는 신의 진짜진짜 비밀',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617d597fab886400139a725c'
+    },
+    {
+        'category': '1분성경',
+        'title': '교회에서는 알려주지 않는 계시록의 불편한 진실',
+        'link': 'https://www.wordsquare.org/video-yard/videos/617d5d5aab886400139a725d'
+    },
+    {
+        'category': '1분성경',
+        'title': '내 인생이 힘들 수 밖에 없는 이유',
+        'link': 'https://www.wordsquare.org/video-yard/videos/6184d3a1ab886400139a72a9'
+    },
+    {
+        'category': '1분성경',
+        'title': '신이 우리에게 자신을 증명한 방법',
+        'link': 'https://www.wordsquare.org/video-yard/videos/6184d403ab886400139a72aa'
+    },
+    {
+        'category': '1분성경',
+        'title': '세기의 논제, 여기서 끝내드립니다!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/6184d438ab886400139a72ab'
+    },
+    {
+        'category': '추천',
+        'title': '타바나(타인이 바라보는 나) 테스트\n※기독교적인 요소가 거의 없으나 컨텐츠의 출처를 알려주며 오픈 할 수 있습니다.',
+        'link': 'https://wordsquare-public.s3.ap-northeast-2.amazonaws.com/assessment/tabana/index.html#/'
     },
 ]
 
 const list2 = [
     {
-        'category': '유쾌한 강사',
-        'title': '왜 원망하죠',
-        'link': 'https://wordsquare.org'
+        'category': '유쾌한강사',
+        'title': '왜 하나님을 원망하죠? 원망할 신은 따로 있는데!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61571206fbd04600149f71c3'
     },
     {
-        'category': '신서의 역사',
-        'title': '이미 이렇게 될 걸 알고 있었다!',
-        'link': 'https://wordsquare.org'
+        'category': '유쾌한강사',
+        'title': '말세의 불심판, 이 영상을 보기 전에는 아는 척 금지',
+        'link': 'https://www.wordsquare.org/video-yard/videos/621b98878f55f400142198f5'
     },
+    {
+        'category': '신서의역사',
+        'title': '성경은 이미 이렇게 될 걸 알고 있었다!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/61570f04fbd04600149f71c1'
+    },
+    {
+        'category': '바이블프리즘',
+        'title': '정말 아담이 첫 사람일까? 호모 사피엔스가 있는데?',
+        'link': 'https://www.wordsquare.org/video-yard/videos/615700a3fbd04600149f71bd'
+    },
+    {
+        'category': '바이블프리즘',
+        'title': 'Oh, MY GOD! 제발 대답 좀 해주세요!',
+        'link': 'https://www.wordsquare.org/video-yard/videos/6220c5448f55f40014219922'
+    },
+    {
+        'category': '추천',
+        'title': '레벨테스트',
+        'link': 'https://www.wordsquare.org/square-crossroads/level-test'
+    },
+
 ]
 
 export {
